a3ae21e177a62777db5545b8653235e5e3a10dc3
Gjystina <xhyndo12@htl-shkoder.com>
2017-06-05 21:38:08 +0200

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,17 +1,14 @@
 		<div id="page">
 		<div id="logo">
-			<h1><a href="/" id="logoLink">BIMA</a></h1>
-			<h2><a href="/" id="logoLink">Use It*****</a></h2>
-			
+			<h1><a href="/" id="logoLink">Wohooooooooooooooooo!</a></h1>
 		</div>
 		<div id="nav">
 			<ul>
 				<li><a href="index.php">Home</a></li>			
 				<li><a href="login.php">Login</a></li>
-				<li><a href="logout.php">Logout</a></li>
+				<li><a href="admin.php">Admin-Area</a></li>
 				<li><a href="register.php">Register</a></li>
-				
-				
-							
+				<li><a href="logout.php">Logout</a></li>
+				<li><a href="destroy.php">SESSION DESTROY - Testing Purpose</a></li>				
 			</ul>	
 		</div>
