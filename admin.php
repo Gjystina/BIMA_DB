028b877506487e3299260e2d169ca10062b90802
samdrohne <samlul12@htl-shkoder.com>
2017-06-05 22:57:26 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <?php require('header.php'); ?>
+   
+   
+    <title>My First Website</title>
+ </head>
+ 
+<body>
+
+<!-- webpage content goes here in the body -->
+        <?php require('menu.php'); ?>
+		<div id="content">
+			<h2>Diese Seite darf nur ein eingeloggter Benutzer sehen!!!</h2>
+		</div>
+		<div id="footer">
+			<p>
+				Webpage made by <a href="/" target="_blank">[your name]</a>
+			</p>
+		</div>
+	</div>
+</body>
+</html>
